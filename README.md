f872cb5af813c6e303d8d18ce83f5b589119e2f1
Kimmo Linna <kimmo.linna@gmail.com>
2023-01-11 13:56:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# pyfingrid
+Fingrid Datahub API for Python
+
+At the moment this is a proof of concept. The API is not yet complete and the code is not yet documented.
+The code uses the [Selenium](https://selenium-python.readthedocs.io/) library and the [requests](https://pypi.org/project/requests/) to scrape the data from the [Fingrid Datahub](https://data.fingrid.fi/). Selenium is used until the authorization and after that requests is used with authorization Token.
+In the future the data is then parsed and returned as a Pandas DataFrame.
+
+## Usage
+
+```python
+python3 pyfingrid.py
+```
---
+++ pyfingrid.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import json,urllib,requests
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://oma.datahub.fi/')
+input("Press any key to continue after authentication")
+s = requests.Session()
+s.cookies.update({c['name']: c['value'] for c in driver.get_cookies()})
+driver.close()
+r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM')
+auth = 'Bearer '+json.loads(urllib.parse.unquote(s.cookies.get_dict()['cap-user']))['token']
+r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM',headers={'Authorization':auth})
+print(r.text)
