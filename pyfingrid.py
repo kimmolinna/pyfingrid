252588f1eb7dac9c6981d06d50c9a77080bec6cd
Kimmo Linna <kimmo.linna@gmail.com>
2023-01-11 13:58:04 +0200

    delete extra line

--- pyfingrid.py
+++ pyfingrid.py
@@ -9,7 +9,6 @@ input("Press any key to continue after authentication")
 s = requests.Session()
 s.cookies.update({c['name']: c['value'] for c in driver.get_cookies()})
 driver.close()
-r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM')
 auth = 'Bearer '+json.loads(urllib.parse.unquote(s.cookies.get_dict()['cap-user']))['token']
 r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM',headers={'Authorization':auth})
-print(r.text)
+print(r.content)
