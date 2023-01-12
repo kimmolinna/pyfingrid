dfbfa1bbc9358ac7dd96aa4217bbc5c4702c6314
Kimmo Linna <kimmo.linna@gmail.com>
2023-01-12 09:24:11 +0200

    webdriver as service and get_token

--- pyfingrid.py
+++ pyfingrid.py
@@ -1,14 +1,34 @@
-from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import json,urllib,requests
+from selenium import webdriver
+import json,urllib,requests,time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+def get_token():
+    driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()))
+    driver.get('https://oma.datahub.fi/')
+    count = 0
+    user = None
+    while count < 60:
+        try:
+            user = {c['name']: c['value'] for c in driver.get_cookies()}['cap-user']
+            driver.close()
+            break
+        except:
+            count += 1
+            time.sleep(1)
+    if user==None:
+        raise Exception('Could not get token')
+    else:
+        return json.loads(urllib.parse.unquote(user))['token']
+def get_metering_points(t : str):
+    """Get metering points from the API
 
-driver.get('https://oma.datahub.fi/')
-input("Press any key to continue after authentication")
-s = requests.Session()
-s.cookies.update({c['name']: c['value'] for c in driver.get_cookies()})
-driver.close()
-auth = 'Bearer '+json.loads(urllib.parse.unquote(s.cookies.get_dict()['cap-user']))['token']
-r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM',headers={'Authorization':auth})
-print(r.content)
+    Arguments:
+        t -- The token from get_token()
+    """
+    s = requests.Session()
+    r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM',headers={'Authorization': 'Bearer '+t})
+    return r.content
+
+token = get_token()
+print(get_metering_points(token))
