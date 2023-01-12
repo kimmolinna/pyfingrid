08414508f07ce900b8c8a86e13e95b2fe2592fc3
Kimmo Linna <kimmo.linna@gmail.com>
2023-01-12 12:09:18 +0200

    start page and cookies

--- pyfingrid.py
+++ pyfingrid.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 import json,requests,time
 import urllib.parse
 
-def get_token():
+def get_cookies():
     """Get a token from the Fingrid datahub API
 
     Raises:
@@ -16,20 +16,28 @@ def get_token():
     driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()))
     driver.get('https://oma.datahub.fi/')
     count = 0
-    user = None
+    cookies = None
     while count < 60:
-        try:
-            user = {c['name']: c['value'] for c in driver.get_cookies()}['cap-user']
-            break
-        except:
-            count += 1
-            time.sleep(1)
-    if user==None:
-        raise Exception('Could not get token')
-    else:
+        count += 1
         time.sleep(1)
-        return json.loads(urllib.parse.unquote(user))['token']
-def login(t : str):
+        if driver.current_url == 'https://oma.datahub.fi/#/':
+            cookies = {c['name']: c['value'] for c in driver.get_cookies()}
+            break
+    if cookies==None:
+        raise Exception('Could not get cookies')
+    else:
+        return cookies
+def get_token(c : dict):
+    """Get a token from the Fingrid datahub API cookies
+    
+    Arguments:
+        c {dict} -- Cookies
+    
+    Returns:
+        str -- The token
+    """
+    return json.loads(urllib.parse.unquote(c['cap-user']))['token']
+def get_session(c: dict):
     """Login to the Fingrid datahub API with a token
 
     Arguments:
@@ -39,7 +47,7 @@ def login(t : str):
         requests.Session -- Session object
     """
     s = requests.Session()
-    r = s.get('https://oma.datahub.fi/',headers={'Authorization': 'Bearer '+t})
+    s.cookies.update(c)
     return s
 def get_metering_points(s : requests.Session, t : str):
     """Get metering points from the API
@@ -57,7 +65,8 @@ def logout(s : requests.Session):
     """
     r = s.get('https://oma.datahub.fi/_api/logout')
     return r.ok
-token = get_token()
-session = login(token)
+cookies = get_cookies()
+session = get_session(cookies)
+token = get_token(cookies)
 print(get_metering_points(session,token))
 ok = logout(session)
