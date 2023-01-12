c051e1a23ab05663f4973b4951e0cc2a65efee15
Kimmo Linna <kimmo.linna@gmail.com>
2023-01-12 10:54:48 +0200

    login, logout

--- pyfingrid.py
+++ pyfingrid.py
@@ -5,6 +5,14 @@ import json,requests,time
 import urllib.parse
 
 def get_token():
+    """Get a token from the Fingrid datahub API
+
+    Raises:
+        Exception: If login is not successful in 60 seconds
+
+    Returns:
+        str -- The token
+    """
     driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()))
     driver.get('https://oma.datahub.fi/')
     count = 0
@@ -12,7 +20,6 @@ def get_token():
     while count < 60:
         try:
             user = {c['name']: c['value'] for c in driver.get_cookies()}['cap-user']
-            driver.close()
             break
         except:
             count += 1
@@ -21,15 +28,35 @@ def get_token():
         raise Exception('Could not get token')
     else:
         return json.loads(urllib.parse.unquote(user))['token']
-def get_metering_points(t : str):
+def login(t : str):
+    """Login to the Fingrid datahub API with a token
+
+    Arguments:
+        t -- The token
+
+    Returns:
+        requests.Session -- Session object
+    """
+    s = requests.Session()
+    r = s.get('https://oma.datahub.fi/',headers={'Authorization': 'Bearer '+t})
+    return s
+def get_metering_points(s : requests.Session, t : str):
     """Get metering points from the API
 
     Arguments:
-        t -- The token from get_token()
+        s {requests.Session} -- Session object
     """
-    s = requests.Session()
     r = s.get('https://oma.datahub.fi/_api/GetAgreementData?agreementStatus=TRM',headers={'Authorization': 'Bearer '+t})
     return r.content
-
+def logout(s : requests.Session):
+    """Logout from the API
+    
+    Arguments:
+        s {requests.Session} -- Session object
+    """
+    r = s.get('https://oma.datahub.fi/_api/logout')
+    return r.ok
 token = get_token()
-print(get_metering_points(token))
+session = login(token)
+print(get_metering_points(session,token))
+ok = logout(session)
