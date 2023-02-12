da0105704f29c3881fece236f846d29cf21734f6
Kimmo Linna <kimmo.linna@gmail.com>
2023-02-12 18:47:55 +0200

    r2

--- datahub.py
+++ datahub.py
@@ -1,6 +1,8 @@
 import pyfingrid as fg
 import pandas as pd
 import awswrangler as wr
+import boto3
+import keyring
 import sys
 import datetime as dt
 
@@ -18,5 +20,7 @@ df = pd.DataFrame(data,columns=['timestamp','consumption'])
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 add = meteringPoints[3][1]
 [add := add.replace(a,b) for a,b in [(' ',''),('.','')]]
-wr.s3.to_parquet(df, "s3://linna/fingrid/"+add+"_"+ str(year) +".parquet")
+b3_session = boto3.Session(profile_name="cloudflare")
+wr.config.s3_endpoint_url = 'https://' + str(keyring.get_password('r2','account_id')) + '.r2.cloudflarestorage.com'
+wr.s3.to_parquet(df, "s3://linna/fingrid/"+add+"_"+ str(year) +".parquet",boto3_session=b3_session)
 fg.logout(session)
