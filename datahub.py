ad8511efe4abbacccb740b08ee3d1619c4940d38
Kimmo Linna <kimmo.linna@gmail.com>
2023-02-14 18:30:03 +0200

    home

--- datahub.py
+++ datahub.py
@@ -18,9 +18,7 @@ meteringPoints = fg.get_metering_points(session)
 data = fg.get_consumption_data(session, meteringPoints[3][0], str(year-1)+'-12-31T22:00', str(year)+'-12-31T22:00')
 df = pd.DataFrame(data,columns=['timestamp','consumption'])
 df['timestamp'] = pd.to_datetime(df['timestamp'])
-add = meteringPoints[3][1]
-[add := add.replace(a,b) for a,b in [(' ',''),('.','')]]
 b3_session = boto3.Session(profile_name="cloudflare")
 wr.config.s3_endpoint_url = 'https://' + str(keyring.get_password('r2','account_id')) + '.r2.cloudflarestorage.com'
-wr.s3.to_parquet(df, "s3://linna/fingrid/"+add+"_"+ str(year) +".parquet",boto3_session=b3_session)
+wr.s3.to_parquet(df, "s3://linna/fingrid/home_"+ str(year) +".parquet",boto3_session=b3_session)
 fg.logout(session)
